c3bdac02b06f2e2d2bd9ac1e4afed0f7a78d5ea8
VictorLima01 <victor.lima.araujo@gmail.com>
2020-03-24 15:41:01 -0300

    Criação de Route Params com o node

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+
+const server = express();
+
+//Query params = ?teste=1
+//Route Params = /users/1
+//Request body = {"name": "Diego", "email" : "diego@rocketseat.com.br"}
+
+
+server.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+
+   return res.json({message: `Buscando o usuário ${id}` });
+})
+
+
+server.listen(3000);
